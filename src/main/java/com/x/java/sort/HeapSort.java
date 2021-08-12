244c191991de3c250736b8fb335cdd79d7993ce4
xuqiang666 <18890285627@163.com>
2021-08-12 21:01:17 +0800

    change db

--- src/main/java/com/x/java/sort/HeapSort.java
+++ src/main/java/com/x/java/sort/HeapSort.java
@@ -7,37 +7,36 @@ import java.util.Arrays;
  */
 public class HeapSort {
 
-    private int[] a ={49,38,65,97,76,13,27,49,78,34,12,64,5,4,62,99,98,54,56,17,18,23,34,15,35,25,53,51};
-
-    public HeapSort(){
+    public HeapSort() {
+        int[] a = {49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17, 18, 23, 34, 15, 35, 25, 53, 51};
         heapSort(a);
     }
 
-    public void heapSort(int[] a){
+    public void heapSort(int[] a) {
         System.out.println("开始排序");
-        int arrayLength=a.length;
+        int arrayLength = a.length;
         //循环建堆
-        for(int i=0;i<arrayLength-1;i++){
+        for (int i = 0; i < arrayLength - 1; i++) {
             //建堆
-            buildMaxHeap(a,arrayLength-1-i);
+            buildMaxHeap(a, arrayLength - 1 - i);
             //交换堆顶和最后一个元素
-            swap(a,0,arrayLength-1-i);
+            swap(a, 0, arrayLength - 1 - i);
             System.out.println(Arrays.toString(a));
         }
     }
 
-    private  void swap(int[] data, int i, int j) {
-        // TODO Auto-generated method stub
-        int tmp=data[i];
-        data[i]=data[j];
-        data[j]=tmp;
+    private void swap(int[] data, int i, int j) {
+        int tmp = data[i];
+        data[i] = data[j];
+        data[j] = tmp;
     }
 
-    /** 对data数组从0到lastIndex建大顶堆 */
+    /**
+     * 对data数组从0到lastIndex建大顶堆
+     */
     private void buildMaxHeap(int[] data, int lastIndex) {
-        // TODO Auto-generated method stub
         //从lastIndex处节点（最后一个节点）的父节点开始
-        for(int i=(lastIndex-1)/2;i>=0;i--) {
+        for (int i = (lastIndex - 1) / 2; i >= 0; i--) {
             //k保存正在判断的节点
             int k = i;
             //如果当前k节点的子节点存在
