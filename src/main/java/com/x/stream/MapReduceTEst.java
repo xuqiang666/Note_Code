9d1ee384f8ecafd87dfca726d48743f7a8b68fb3
xuqiang666 <18890285627@163.com>
2022-03-23 16:44:20 +0800

    map、reduce

--- src/main/java/com/x/stream/MapReduceTEst.java
+++ src/main/java/com/x/stream/MapReduceTEst.java
@@ -2,14 +2,29 @@ package com.x.stream;
 
 import com.google.common.collect.Lists;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class MapReduceTEst {
     public static void main(String[] args) {
         List<String> list = Lists.newArrayList("我阿达发 ", "答复啊", "而非微软给", "而我认为");
         String s = list.stream().map(a -> a + "\n")
                 .reduce((a, b) -> a + "" + b)
-                .get();
+                .orElse("");
         System.out.println(s);
+
+
+        List<User> userList = Arrays.asList(
+                new User(1, "拉拉"),
+                new User(2, "two"),
+                new User(3, "第三人")
+        );
+        int totalAge = userList.stream().mapToInt(User::getAge).sum();
+        String names = userList.stream().map(User::getName).reduce((a, b) -> a + "," + b).orElse("");
+        BigDecimal totalMoney = userList.stream().map(User::getMoney).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+
+        System.out.println(names + ";" + totalAge + ";" + totalMoney);
     }
 }
--- src/main/java/com/x/stream/User.java
+++ src/main/java/com/x/stream/User.java
@@ -1,15 +1,23 @@
 package com.x.stream;
 
+import java.math.BigDecimal;
+import java.util.Random;
+
 /**
  * @Author: 许庆之 on 2020/11/20.
  */
 public class User {
     private int id;
     private String name;
+    private BigDecimal money;
+    private int age;
 
     public User(int id, String name) {
         this.id = id;
         this.name = name;
+        //0到n之间的随机int值，包含0而不包含n
+        this.age = new Random().nextInt(100);
+        this.money = BigDecimal.valueOf(new Random().nextInt(100)).add(new BigDecimal("0.11"));
     }
 
     public int getId() {
@@ -26,5 +34,21 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
     }
 }
